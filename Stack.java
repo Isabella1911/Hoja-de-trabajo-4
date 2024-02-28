7a20560e8f56e06af4b541c718c41bc131409785
Isabella1911 <139140580+Isabella1911@users.noreply.github.com>
2024-02-27 19:51:44 -0600

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,3 +1,7 @@
+/*Universidad del Valle de Guatemala
+Algoritmos y estruccturas de datos
+Linda Chen 23173
+Isabella Obando 23074*/
 public interface Stack<T> {
     void push(T element);
     T pop();
